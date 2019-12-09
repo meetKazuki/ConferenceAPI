840ca0126c79b39cc7d32ae03e788d104c984acf
Desmond Edem <edemintech@gmail.com>
2019-12-09 02:26:46 +0100

    chore(mock): add mock data for talk events

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,38 @@
 const DB = {
   attendees: [],
 
-  talks: [],
+  talks: [
+    {
+      id: 1,
+      topic: 'envisioneer value-added e-markets',
+      location: 'West',
+      hostId: 1,
+    },
+    {
+      id: 2,
+      topic: 'transform cutting-edge experiences',
+      location: 'Oakridge',
+      hostId: 2,
+    },
+    {
+      id: 3,
+      topic: 'strategize B2C technologies',
+      location: 'Crescent Oaks',
+      hostId: 3,
+    },
+    {
+      id: 4,
+      topic: 'revolutionize holistic supply-chains',
+      location: 'Springview',
+      hostId: 4,
+    },
+    {
+      id: 5,
+      topic: 'scale integrated e-markets',
+      location: 'Homewood',
+      hostId: 5,
+    },
+  ],
 };
 
 export default DB;
